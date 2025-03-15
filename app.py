70ddeff37c7eabdd80d1af47ba1440dcb52c5b2d
Guihauck <guihauck1998@gmail.com>
2025-03-15 15:45:31 -0300

    Inserção de método e rota para pesquisa do ID da atividade

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# CRUD: Create, Read, Update and Delete - > Criar, Ler, Atualizar, Deletar
+# CRUD: Create, Read, Update and Delete - > Criar, Ler, Atualizar, Deletar.
 from flask import Flask, request, jsonify
 from model.tasks import Task
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 tasks = list()
 tasks_id_control = 1
 
-@app.route("/tasks", methods=['POST']) # Traçando rota método POST
+@app.route("/tasks", methods=['POST']) # Traçando rota método POST.
 def create_task():
   global tasks_id_control
   data = request.get_json()
@@ -17,16 +17,24 @@ def create_task():
   print(tasks, data)
   return jsonify({"Mensagem": "Nova Tarefa Criada"})
 
-@app.route("/tasks", methods=['GET'])
-def get_task():
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
     task_list = []
     for task in tasks:
        task_list.append(task.to_dict()) # Adicionado o formato dicionário.
     output = {
           "tasks": task_list,
-          "total_tasks": 0
+          "total_tasks": len(task_list) # Contagem dos elementos na lista.
         }
     return jsonify(output)
 
+@app.route('/task/<int:id>', methods=['GET'])
+def get_task(id):
+  for t in tasks:
+   if t.id == id:
+      return jsonify(t.to_dict())
+   
+  return jsonify({"Mensagem": "Não foi possível carregar a atividade."}), 404
+
 if __name__ == "__main__":
-  app.run(debug=True) #Habilitação de logs. 
+  app.run(debug=True) #Habilitação de logs.  
--- model/tasks.py
+++ model/tasks.py
@@ -1,11 +1,11 @@
 class Task:
   def __init__(self, id, title, description, completed=False):
-    self.id = id,
-    self.title = title,
-    self.description = description,
+    self.id = id
+    self.title = title
+    self.description = description
     self.completed = completed # Definição dos atributos utilizando self.
 
-  def to_dict(self): # Função que retorna o dicionário de dados dos atributos.
+  def to_dict(self): # Método que retorna um dicionário com os atributos
     return {
       "id": self.id,
       "title": self.title,
