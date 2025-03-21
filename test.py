ad5370c3f9c0789cf75b47eb9da05401e6ac6a98
Guihauck <guihauck1998@gmail.com>
2025-03-21 14:07:26 -0300

    Implementação de todos os testes automatizados

--- test.py
+++ test.py
@@ -24,3 +24,40 @@ def test_read_get_task():
   response_json = response.json()
   assert "tasks" in response_json
   assert "total_tasks" in response_json
+
+def test_read_get_tasks():
+  if tasks:
+    task_id = tasks[0]
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert task_id == response_json['id']
+
+def test_tasks_update():
+  if tasks:
+    task_id = tasks[0]
+    payload = {
+      "completed": True,
+      "description": "A tarefa atualizada pelo teste automatizado.",
+      "title": "Tarefa atualizada pelo teste"
+    }
+    response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+    response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+
+# Nova requisição a tarefa específica.
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json['title'] == payload['title']
+    assert response_json['description'] == payload['description']
+    assert response_json['completed'] == payload['completed']
+
+def task_task_delete():
+  if tasks:
+    task_id = tasks[0]
+    response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+    response.status_code == 200
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 404
